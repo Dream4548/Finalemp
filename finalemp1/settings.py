ccadb65621318237e29dcd5e1f6d137b78b7fbc4
Dream4548 <deemgamer123@gmail.com>
2023-10-02 14:25:34 +0700

    vercel3

--- finalemp1/settings.py
+++ finalemp1/settings.py
@@ -125,7 +125,7 @@ import os
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'statics', ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
